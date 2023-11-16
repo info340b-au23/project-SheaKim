3198a22181c2a33f87c8827d9288953a0ce109c9
SheaKim <114708841+SheaKim@users.noreply.github.com>
2023-11-16 00:01:04 -0800

    added footer.js, started about page component

---
+++ src/components/About.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function AboutPage() {
+    return (
+        <h2>Our Mission</h2>
+            <p>Description of web app and why we wanted to build it</p>
+        h2>Authors</h2>
+            <p>Description of authors</p>
+}
