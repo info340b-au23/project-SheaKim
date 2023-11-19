2a01758a6bb5664265fe94dd7c0f0fd23461de02
vvaibava <65467493+vvaibava@users.noreply.github.com>
2023-11-19 13:12:38 -0800

    changes to sicklog

--- src/components/Sickness Log.js
+++ src/components/Sickness Log.js
@@ -1,13 +1,64 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Footer from './Footer';
+import Header from './Header'
 
-export default function Footer() {
-    return (
-        <div className="footer-container">
-        <footer>
-            <p><a href="mailto:email@sickness"><span class="material-icons">email</span> email@sickness.uw.edu</a></p>
-            <p><a href="tel:555-123-4567"><span class="material-icons">phone</span> 555-123-4567</a></p>
-            <p>&copy; Sickness Tracker</p>
-        </footer>
+const sickLog = () => {
+  const [logData, setLogData] = useState({
+      symptoms: '',
+      description: '',
+      date: '',
+      comments: '',
+    });
+    
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setLogData({ ...logData, [name]: value });
+  };
+      
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (logData.symptoms.trim() === '' || logData.description.trim() === '' || logData.date === '') {
+      return;
+    }
+    const newLog = {
+      id: Date.now(),
+      symptoms: logData.symptoms,
+      description: logData.description,
+      date: logData.date,
+      comments: logData.comments,
+    };
+    setLogs([...logs, newLog]);
+    setLogData({ 
+      symptoms: '',
+      description: '',
+      date: '',
+      comments: '',
+    });
+  };
+
+  return (
+    <div>
+      <Header /> 
+      <main>
+        <form className="sicktrkr" onSubmit={handleSubmit}>
+          <label>Symptoms:</label>
+          <textarea value={logData.symptoms} onChange={handleInputChange}></textarea>
+          <label>Description:</label>
+          <textarea value={logData.description} onChange={handleInputChange}></textarea>
+          <label>Date of Notice:</label>
+          <input type="date" value={logData.date} onChange={handleInputChange} />
+          <label>Extra Comments:</label>
+          <textarea value={logData.comments} onChange={handleInputChange}></textarea>
+          <button type="submit">Log Sickness</button>
+        </form>
+        <div>
         </div>
-    )
-}
+      </main>
+      <Footer />
+    </div>
+  );
+};
+
+export default sickLog;
