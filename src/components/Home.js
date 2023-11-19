7c2d70a214b72fae7336b653ae82d7bcc4275961
andrewchiang3 <andrewchiang3@gmail.com>
2023-11-19 12:11:25 -0800

    added home page html

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,12 +2,86 @@ import React from 'react';
 
 export default function Footer() {
     return (
-        <div className="footer-container">
-        <footer>
-            <p><a href="mailto:email@sickness"><span class="material-icons">email</span> email@sickness.uw.edu</a></p>
-            <p><a href="tel:555-123-4567"><span class="material-icons">phone</span> 555-123-4567</a></p>
-            <p>&copy; Sickness Tracker</p>
-        </footer>
-        </div>
+        <main>
+            <section>
+                <div class="slogan">
+                    <h2>Your Wingman to Health</h2>
+                    <p>Track your medication, log your sickness, and find resources to support your health</p>
+                    <button>Login</button>
+                    <button>Sign Up</button>
+                    
+                </div>
+            </section>
+
+            <section class="col_two space">
+                <h2>Perfect If You Are</h2>
+                <div class="flex-container">
+                    <div class="card border-0">
+                        
+                            <p>Monitoring Your Own Health</p>
+                    </div>
+                    <div class="card border-0">
+                        
+                            <p>Caring For Loved Ones</p>
+                    </div>
+                    <div class="card border-0">
+                        
+                            <p>Searching For Diagnosis</p>
+                    </div>
+                </div>
+            </section>
+
+            <div class="examples">
+                <section>
+                    <div class="container">
+                        <div class="example-description">
+                            <h4>Medication Tracker</h4>
+                            <p>Track your dosage, visualize your plan, schedule refills</p>
+                        </div>
+                        
+                    </div>
+
+                </section>
+
+                <section>
+                    <div class="container">
+                        <div class="example-description">
+                            <h4>Log Sickness</h4>
+                            <p>Track symptoms, log dates, visualize data</p>
+                        </div>
+                        
+                    </div>
+                </section>
+
+                <section>
+                    <div class="container last">
+                        <div class="example-description">
+                            <h4>Find Resources</h4>
+                            <p>Enter your symptoms, find resources</p>
+                        </div>
+                        
+                    </div>
+                </section>
+            </div>
+
+            <div class="summary">
+                <section>
+                    <h4>Summarize Your Health</h4>
+                    <p>See simple visualizations of your sickness history</p>
+                    
+                </section>
+            </div>
+
+
+            <div class="overview">
+                <section>
+                    <div class="container">
+                        <h4>Your Simple Sickness Tracker</h4>
+                        <p>Make Health Easier</p>
+                        
+                    </div>
+                </section>
+            </div>
+        </main >
     )
 }
