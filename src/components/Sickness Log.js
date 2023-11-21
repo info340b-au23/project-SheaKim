14cdbbee7c2632ab2330e5cbf748304625b36c6b
vvaibava <65467493+vvaibava@users.noreply.github.com>
2023-11-21 14:51:52 -0800

    Update Sickness Log.js

--- src/components/Sickness Log.js
+++ src/components/Sickness Log.js
@@ -14,7 +14,6 @@ const SickLog = () => {
   
   const handleInputChange = (event) => {
     const { name, value } = event.target;
-    console.log(name, value); 
     setLogData({ ...logData, [name]: value });
   }
       
@@ -45,13 +44,13 @@ const SickLog = () => {
       <main>
         <form className="sicktrkr" onSubmit={handleSubmit}>
           <label>Symptoms:</label>
-          <textarea value={logData.symptoms} onChange={handleInputChange}></textarea>
+          <textarea name="symptoms" value={logData.symptoms} onChange={handleInputChange}></textarea>
           <label>Description:</label>
-          <textarea value={logData.description} onChange={handleInputChange}></textarea>
+          <textarea name="description" value={logData.description} onChange={handleInputChange}></textarea>
           <label>Date of Notice:</label>
-          <input type="date" value={logData.date} onChange={handleInputChange} />
+          <input name="date" type="date" value={logData.date} onChange={handleInputChange} />
           <label>Extra Comments:</label>
-          <textarea value={logData.comments} onChange={handleInputChange}></textarea>
+          <textarea name="comments" value={logData.comments} onChange={handleInputChange}></textarea>
           <button type="submit">Log Sickness</button>
         </form>
         <div>
