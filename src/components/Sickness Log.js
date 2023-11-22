b99081adf01191edeeb6b02e70e4a8030e2506f9
vvaibava <65467493+vvaibava@users.noreply.github.com>
2023-11-21 18:19:14 -0800

    Update Sickness Log.js

--- src/components/Sickness Log.js
+++ src/components/Sickness Log.js
@@ -40,9 +40,9 @@ const SickLog = () => {
           <label>Symptoms:</label>
           <textarea name="symptoms" value={logData.symptoms} onChange={handleInputChange}></textarea>
           <label>Description:</label>
-          <textarea name="description" value={logData.description} onChange={handleInputChange}  placeholder="YYYY-MM-DD"></textarea>
+          <textarea name="description" value={logData.description} onChange={handleInputChange}></textarea>
           <label>Date of Notice:</label>
-          <input name="date" type="date" value={logData.date} onChange={handleInputChange} />
+          <input name="date" type="date" value={logData.date} onChange={handleInputChange}></input>
           <label>Extra Comments:</label>
           <textarea name="comments" value={logData.comments} onChange={handleInputChange}></textarea>
           <button type="submit">Log Sickness</button>
