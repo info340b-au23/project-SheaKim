fbf333ce8481ece966238a5b0338e1e97f9e16bc
SheaKim <114708841+SheaKim@users.noreply.github.com>
2023-12-11 10:13:32 -0800

    api changes

--- src/components/Med Tracker.js
+++ src/components/Med Tracker.js
@@ -5,6 +5,7 @@ import { useGoogleLogin } from '@react-oauth/google';
 //import { GoogleLogin } from '@react-oauth/google';
 import { SyntheticEvent } from 'react';
 import ApiCalendar from "react-google-calendar-api";
+import React, { useEffect } from "react";
 
 
 export default function MedTracker() {
@@ -93,39 +94,362 @@ export default function MedTracker() {
        
        };
   
-  const responseMessage = (response) => {
-        console.log(response);
-    };
-  const errorMessage = (error) => {
-        console.log(error);
-    };
+  // const responseMessage = (response) => {
+  //       console.log(response);
+  //   };
+  // const errorMessage = (error) => {
+  //       console.log(error);
+  //   };
 
-  const login = useGoogleLogin({
-    onSuccess: codeResponse => console.log(codeResponse),
-  flow: 'auth-code',
-  });
+  // const login = useGoogleLogin({
+  //   onSuccess: codeResponse => console.log(codeResponse),
+  // flow: 'auth-code',
+  // });
 
-  const config = {
-    clientId: "737643364541-a4lmervpmj6jubdqm2n0ag2084g4abk3.apps.googleusercontent.com",
-    apiKey: "AIzaSyBcSh5szoNsvvz7SJ3zvPP0q455hjVa7yU",
-    scope: "https://www.googleapis.com/auth/calendar",
-    discoveryDocs: [
-      "https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest",
-    ],
-  };
+  // const config = {
+  //   clientId: "737643364541-a4lmervpmj6jubdqm2n0ag2084g4abk3.apps.googleusercontent.com",
+  //   apiKey: "AIzaSyBcSh5szoNsvvz7SJ3zvPP0q455hjVa7yU",
+  //   scope: "https://www.googleapis.com/auth/calendar",
+  //   discoveryDocs: [
+  //     "https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest",
+  //   ],
+  // };
   
-  const apiCalendar = new ApiCalendar(config);
+  // const apiCalendar = new ApiCalendar(config);
 
-  const TestDemo = () => {
-    const [events, setEvents] = useState([]);
-    const [calendars, setCalendars] = useState([]);
-    const handleItemClick = (event, name) => {
-      if (name === 'sign-in') {
-        apiCalendar.handleAuthClick()
-      } else if (name === 'sign-out') {
-        apiCalendar.handleSignoutClick();
-      }
-    };
+  // const TestDemo = () => {
+  //   const [events, setEvents] = useState([]);
+  //   const [calendars, setCalendars] = useState([]);
+  //   const handleItemClick = (event, name) => {
+  //     if (name === 'sign-in') {
+  //       apiCalendar.handleAuthClick()
+  //     } else if (name === 'sign-out') {
+  //       apiCalendar.handleSignoutClick();
+  //     }
+  //   };
+
+
+  const gapi = window.gapi;
+
+const google = window.google;
+
+ 
+
+const CLIENT_ID =
+
+"1084837142274-e4f0f7st0bb948hgvd40o9b7cg6g11ao.apps.googleusercontent.com";
+
+const API_KEY = "AIzaSyD7_-bhPqIKNlnJN8OvPYAUql2Uyl5fD0Q";
+
+const DISCOVERY_DOC =
+
+"https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest";
+
+const SCOPES = "https://www.googleapis.com/auth/calendar";
+
+ 
+
+const accessToken = localStorage.getItem("access_token");
+
+const expiresIn = localStorage.getItem("expires_in");
+
+ 
+
+let gapiInited = false,
+
+gisInited = false,
+
+tokenClient;
+
+ 
+
+useEffect(() => {
+
+//const expiryTime = new Date().getTime() + expiresIn * 1000;
+
+gapiLoaded();
+
+gisLoaded();
+
+}, []);
+
+ 
+
+function gapiLoaded() {
+
+gapi.load("client", initializeGapiClient);
+
+}
+
+ 
+
+async function initializeGapiClient() {
+
+await gapi.client.init({
+
+apiKey: API_KEY,
+
+discoveryDocs: [DISCOVERY_DOC],
+
+});
+
+gapiInited = true;
+
+ 
+
+if (accessToken && expiresIn) {
+
+gapi.client.setToken({
+
+access_token: accessToken,
+
+expires_in: expiresIn,
+
+});
+
+listUpcomingEvents();
+
+}
+
+}
+
+ 
+
+function gisLoaded() {
+
+tokenClient = google.accounts.oauth2.initTokenClient({
+
+client_id: CLIENT_ID,
+
+scope: SCOPES,
+
+DISCOVERY_DOC:
+
+"https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest",
+
+callback: "", // defined later
+
+});
+
+ 
+
+gisInited = true;
+
+}
+
+ 
+
+//Enables user interaction after all libraries are loaded.
+
+ 
+
+function handleAuthClick() {
+
+tokenClient.callback = async (resp) => {
+
+if (resp.error) {
+
+throw resp;
+
+}
+
+await listUpcomingEvents();
+
+const { access_token, expires_in } = gapi.client.getToken();
+
+localStorage.setItem("access_token", access_token);
+
+localStorage.setItem("expires_in", expires_in);
+
+};
+
+ 
+
+if (!(accessToken && expiresIn)) {
+
+// Prompt the user to select a Google Account and ask for consent to share their data
+
+// when establishing a new session.
+
+tokenClient.requestAccessToken({ prompt: "consent" });
+
+} else {
+
+// Skip display of account chooser and consent dialog for an existing session.
+
+tokenClient.requestAccessToken({ prompt: "" });
+
+}
+
+}
+
+ 
+
+//Sign out the user upon button click.
+
+ 
+
+function handleSignoutClick() {
+
+const token = gapi.client.getToken();
+
+if (token !== null) {
+
+google.accounts.oauth2.revoke(token.access_token);
+
+gapi.client.setToken("");
+
+localStorage.clear();
+
+}
+
+}
+
+ 
+
+async function listUpcomingEvents() {
+
+let response;
+
+try {
+
+const request = {
+
+calendarId: "primary",
+
+timeMin: new Date().toISOString(),
+
+showDeleted: false,
+
+singleEvents: true,
+
+maxResults: 10,
+
+orderBy: "startTime",
+
+};
+
+response = await gapi.client.calendar.events.list(request);
+
+} catch (err) {
+
+document.getElementById("content").innerText = err.message;
+
+return;
+
+}
+
+ 
+
+const events = response.result.items;
+
+if (!events || events.length === 0) {
+
+document.getElementById("content").innerText = "No events found.";
+
+return;
+
+}
+
+// Flatten to string to display
+
+const output = events.reduce(
+
+(str, event) =>
+
+`${str}${event.summary} (${
+
+event.start.dateTime || event.start.date
+
+})\n`,
+
+"Events:\n"
+
+);
+
+document.getElementById("content").innerText = output;
+
+}
+
+ 
+
+function addManualEvent() {
+
+var event = {
+
+kind: "calendar#event",
+
+summary: "Event 2",
+
+location: "Masai School, Bangalore",
+
+description: "Paty time",
+
+start: {
+
+dateTime: "2023-07-18T01:05:00.000Z",
+
+timeZone: "UTC",
+
+},
+
+end: {
+
+dateTime: "2023-07-18T01:35:00.000Z",
+
+timeZone: "UTC",
+
+},
+
+recurrence: ["RRULE:FREQ=DAILY;COUNT=1"],
+
+attendees: [
+
+{ email: "techmovieadd@gmail.com", responseStatus: "needsAction" },
+
+],
+
+reminders: {
+
+useDefault: true,
+
+},
+
+guestsCanSeeOtherGuests: true,
+
+};
+
+ 
+
+var request = gapi.client.calendar.events.insert({
+
+calendarId: "primary",
+
+resource: event,
+
+sendUpdates: "all",
+
+});
+
+request.execute(
+
+(event) => {
+
+console.log(event);
+
+window.open(event.htmlLink);
+
+},
+
+(error) => {
+
+console.error(error);
+
+}
+
+);
+
+}
 
   return (
     <body>
@@ -256,9 +580,57 @@ export default function MedTracker() {
     <button onClick={() => login()}>Sign in with Google 🚀</button>
     </GoogleOAuthProvider>
 
-    </div> */}
+  </div> */}
 
-    <div>
+<div>
+
+<button
+
+id="authorize_button"
+
+hidden={accessToken && expiresIn}
+
+onClick={handleAuthClick}
+
+>
+
+Authorize
+
+</button>
+
+<button
+
+id="signout_button"
+
+hidden={!accessToken && !expiresIn}
+
+onClick={handleSignoutClick}
+
+>
+
+Sign Out
+
+</button>
+
+<button
+
+id="add_manual_event"
+
+hidden={!accessToken && !expiresIn}
+
+onClick={addManualEvent}
+
+>
+
+Add Event
+
+</button>
+
+<pre id="content" style={{ whiteSpace: "pre-wrap" }}></pre>
+
+</div>
+
+    {/* <div>
       <div style={{ padding: "0.5em" }}>
         <button onClick={(e) => handleItemClick(e, "sign-in")}>sign-in</button>
         <button onClick={(e) => handleItemClick(e, "sign-out")}>
@@ -329,7 +701,7 @@ export default function MedTracker() {
           Create calendar
         </button>
        </div>
-    </div>
+    </div> */}
 
 
     <div className="MedTracker">
@@ -418,6 +790,4 @@ export default function MedTracker() {
 </body>
 
   );
-}
-
 }
