2e2381cf6094409da262cb0093e5967c9ccb4656
SheaKim <114708841+SheaKim@users.noreply.github.com>
2023-12-11 09:42:58 -0800

    api work

--- src/components/Med Tracker.js
+++ src/components/Med Tracker.js
@@ -2,11 +2,14 @@ import { useState } from 'react';
 import calendar from '../img/google-calendar.png';
 import { GoogleOAuthProvider } from '@react-oauth/google';
 import { useGoogleLogin } from '@react-oauth/google';
-import { GoogleLogin } from '@react-oauth/google';
+//import { GoogleLogin } from '@react-oauth/google';
+import { SyntheticEvent } from 'react';
+import ApiCalendar from "react-google-calendar-api";
 
 
 export default function MedTracker() {
-    let tabs = document.querySelectorAll(".tabs h3");
+
+  let tabs = document.querySelectorAll(".tabs h3");
     let tabContents = document.querySelectorAll(".tab-content div");
     tabs.forEach((tab, index) => {
         tab.addEventListener("click", () => {
@@ -101,6 +104,28 @@ export default function MedTracker() {
     onSuccess: codeResponse => console.log(codeResponse),
   flow: 'auth-code',
   });
+
+  const config = {
+    clientId: "737643364541-a4lmervpmj6jubdqm2n0ag2084g4abk3.apps.googleusercontent.com",
+    apiKey: "AIzaSyBcSh5szoNsvvz7SJ3zvPP0q455hjVa7yU",
+    scope: "https://www.googleapis.com/auth/calendar",
+    discoveryDocs: [
+      "https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest",
+    ],
+  };
+  
+  const apiCalendar = new ApiCalendar(config);
+
+  const TestDemo = () => {
+    const [events, setEvents] = useState([]);
+    const [calendars, setCalendars] = useState([]);
+    const handleItemClick = (event, name) => {
+      if (name === 'sign-in') {
+        apiCalendar.handleAuthClick()
+      } else if (name === 'sign-out') {
+        apiCalendar.handleSignoutClick();
+      }
+    };
 
   return (
     <body>
@@ -226,22 +251,86 @@ export default function MedTracker() {
                 </ol>
               </div>
     
-    <div class="calendar">
-    {/* <GoogleLogin onSuccess={responseMessage} onError={errorMessage} /> */}
+    {/* <div class="calendar">
     <GoogleOAuthProvider clientId="737643364541-a4lmervpmj6jubdqm2n0ag2084g4abk3.apps.googleusercontent.com">
-    {/* <GoogleLogin
-  onSuccess={credentialResponse => {
-    console.log(credentialResponse);
-  }}
-  onError={() => {
-    console.log('Login Failed');
-  }}
-/> */}
     <button onClick={() => login()}>Sign in with Google 🚀</button>
     </GoogleOAuthProvider>
 
+    </div> */}
+
+    <div>
+      <div style={{ padding: "0.5em" }}>
+        <button onClick={(e) => handleItemClick(e, "sign-in")}>sign-in</button>
+        <button onClick={(e) => handleItemClick(e, "sign-out")}>
+          sign-out
+        </button>
+      </div>
+      <div style={{ padding: "0.5em" }}>
+        <button
+          onClick={(e) => {
+            const eventFromNow = {
+              summary: "Poc Dev From Now",
+              time: 480,
+            };
+
+            apiCalendar
+              .createEventFromNow(eventFromNow)
+              .then((result) => {
+                console.log(result);
+              })
+              .catch((error) => {
+                console.log(error);
+              });
+          }}
+        >
+          Create Event from now
+        </button>
+      </div>
+      <div style={{ padding: "0.5em" }}>
+        <button
+          onClick={(e) => {
+            apiCalendar.listUpcomingEvents(10).then(({ result }) => {
+              console.log(result.items);
+              setEvents(result.items);
+            });
+          }}
+        >
+          List upcoming events
+        </button>
+        <div>
+          <h4>Events</h4>
+          {events.length === 0 && <p>No events to show</p>}
+          {events.map((event) => (
+            <p key={event.id}>{JSON.stringify(event)}</p>
+          ))}
+        </div>
+      </div>
+      <div style={{ padding: "0.5em" }}>
+        <button
+          onClick={(e) => {
+            apiCalendar.listCalendars().then(({ result }) => {
+              console.log(result.items);
+              setCalendars(result.items);
+            });
+          }}
+        >
+          List calendars
+        </button>
+
+      </div>
+      <div style={{ padding: "0.5em" }}>
+        <button
+          onClick={(e) => {
+            apiCalendar.createCalendar("myCalendar2").then(({ result }) => {
+              console.log(result);
+            });
+          }}
+        >
+          Create calendar
+        </button>
+       </div>
     </div>
-            
+
 
     <div className="MedTracker">
       <form onSubmit={submit}>
@@ -329,4 +418,6 @@ export default function MedTracker() {
 </body>
 
   );
+}
+
 }
